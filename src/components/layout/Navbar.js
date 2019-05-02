baa48509c89ba79e8b95d8081d4167de3912061d
uknowcreation <uknowcreation@gmail.com>
2019-05-02 18:15:55 +0200

    feat(layou navbar): create navbar component content

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Brand } from '../layout';
+import Brand from '../layout/Brand';
+
+import './css/Layout.css';
+import './css/Navbar.css';
 
 class Navbar extends React.Component {
     render() {
       return(
         <div className="navbar navbar-expand-md navbar-light">
           <Brand />
+          <div className="collapse navbar-collapse content-boxed">
+
+          </div>
         </div>
       );
     }
