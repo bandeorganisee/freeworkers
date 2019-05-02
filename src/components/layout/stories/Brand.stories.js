076f5636f3fe78afd4c58b0cd3ba3a6dd2f37dfb
uknowcreation <uknowcreation@gmail.com>
2019-05-02 18:19:47 +0200

    story(layout brand): update brand storie

--- src/components/layout/stories/Brand.stories.js
+++ src/components/layout/stories/Brand.stories.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import Brand from '../Brand';
 
-storiesOf('Layout', module)
-  .add('Brand', () => <Brand ></Brand>)
+const stories = storiesOf('Layout', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Brand', () => <Brand ></Brand>)
