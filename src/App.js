d7bc5255fc7725369b9ecdf13aefff8f49f3eac8
uknowcreation <uknowcreation@gmail.com>
2019-05-06 10:11:03 +0200

    feat(app): add css file into app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import './assets/ui/variable.css';
+import './assets/ui/style.css';
 import Navbar from './components/layout/Navbar';
 
 class App extends Component {
