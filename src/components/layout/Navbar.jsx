eadba3861b7c81bec04ccbc546fbee79c661d704
uknowcreation <uknowcreation@gmail.com>
2019-05-19 11:34:06 +0200

    chore(components header): change extension file js to jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Brand from '../layout/Brand';
 
 import './css/Layout.css';
 import './css/Navbar.css';
@@ -7,10 +6,8 @@ import './css/Navbar.css';
 class Navbar extends React.Component {
     render() {
       return(
-        <div className="navbar navbar-expand-md navbar-light">
-          <Brand />
-          <div className="collapse navbar-collapse content-boxed">
-          </div>
+        <div className="collapse navbar-collapse content-boxed">
+
         </div>
       );
     }
