19720879f61958e66be19a1f01e304b8c0d25880
uknowcreation <uknowcreation@gmail.com>
2019-05-19 11:33:38 +0200

    chore(component brand): change extension file js to jsx for react

--- src/components/layout/Brand.jsx
+++ src/components/layout/Brand.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Logo from '../../assets/img/logo.png';
-import './css/Layout.css';
 import './css/Brand.css';
 
 class Brand extends React.Component {
   render() {
     return(
-      <div className="navbar-header navbar-dark d-none d-md-flex align-items-md-center">
-        <div className="navbar-brand navbar-brand-md">
+      <div className="brand-header brand-container d-none d-md-flex align-items-md-center">
+        <div className="navbar-brand brand-content navbar-brand-md">
           <a href="/" className="d-inline-block">
             <img src={Logo}  alt="Logo"/>
           </a>
