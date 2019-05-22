67ddf98e38a975d618fb006bf8e11b0cee909180
uknowcreation <uknowcreation@gmail.com>
2019-05-22 21:48:37 +0200

    feat(components sidebar): create sidebar component

---
+++ src/components/layout/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+import './css/Sidebar.scss';
+
+class Sidebar extends React.Component {
+  render() {
+    return(
+      <div className="sidebar sidebar-dark sidebar-main sidebar-expand-md">
+        <div className="sidebar-content">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
