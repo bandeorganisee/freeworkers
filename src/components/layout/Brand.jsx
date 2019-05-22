d307e6fd6dc270f98771a4fc753b2bb461610a98
uknowcreation <uknowcreation@gmail.com>
2019-05-22 21:44:57 +0200

    feat(components brand): add props for logo and description

--- src/components/layout/Brand.jsx
+++ src/components/layout/Brand.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import Logo from '../../assets/img/logo.png';
-import './css/Brand.css';
+import './css/Brand.scss';
 
 class Brand extends React.Component {
   render() {
@@ -9,12 +9,29 @@ class Brand extends React.Component {
       <div className="brand-header brand-container d-none d-md-flex align-items-md-center">
         <div className="navbar-brand brand-content navbar-brand-md">
           <a href="/" className="d-inline-block">
-            <img src={Logo}  alt="Logo"/>
+            <img
+              src={ this.props.brandLogo }
+              alt={ this.props.brandDescription }
+              width= { this.props.widthBrand }
+              height= { this.props.heightBrand }
+            />
           </a>
         </div>
       </div>
     )
   }
+}
+
+Brand.propTypes = {
+  brandLogo: PropTypes.string,
+  brandDescription: PropTypes.string,
+  widthBrand: PropTypes.number,
+  heightBrand: PropTypes.number,
+}
+
+Brand.defaultProps = {
+  brandLogo: 'https://i.goopics.net/92K7V.png',
+  brandDescription: 'Your logo Here'
 }
 
 export default Brand;
