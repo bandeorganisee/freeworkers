95af5e862cfd6163478a3d5cd8254ad815f4089d
uknowcreation <uknowcreation@gmail.com>
2019-05-22 21:49:20 +0200

    feat(app): import sidebar component into app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './assets/variable.css';
 import './assets/style.css';
 import Header from './components/layout/Header.jsx';
+import Sidebar from './components/layout/Sidebar.jsx';
 
 class App extends Component {
     render() {
       return (
         <div>
           <Header />
+          <Sidebar />
         </div>
       )
     }
